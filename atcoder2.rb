7fc3506a9e51c703bea05ebf5f1ac3b10afd4350
showma0734 <showma0734@gmail.com>
2020-03-30 14:58:36 +0900

    change 4 line"

--- atcoder2.rb
+++ atcoder2.rb
@@ -1,12 +1,12 @@
 def marbles(a,b,c)
     d = [a,b,c]
-    
-    if d.all?{|x| x.equal?(0) || x.equal?(1) && x.is_a?(Integer)}  
-        p d.sum
-        #d.inject(0){ |cnt,n| cnt += n }
-    else
-        nil
+    d.each do |x|
+        unless ((x == 1 || x == 0 && x.is_a?(Integer)))
+            return nil
+        end
     end
+    p d.sum
+    #d.inject(0){ |cnt,n| cnt += n }git
 end
 
 puts marbles( 1, 0, 1) == 2
@@ -14,5 +14,7 @@ puts marbles( 0, 0, 0) == 0
 puts marbles( 2, 0, 1) == nil
 puts marbles("a" ,"a" ,"a") == nil
 puts marbles("a", 0, 1) == nil
+
+
 
 
