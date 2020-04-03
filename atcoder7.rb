281a07546a647d338c5bea3707b60bae4a7f3df2
showma0734 <showma0734@gmail.com>
2020-04-03 16:57:53 +0900

    add n variable

--- atcoder7.rb
+++ atcoder7.rb
@@ -1,23 +1,28 @@
-def valid?(*mochi)
+def valid?(n,*mochi)
+  return false unless n.is_a?(Integer)
+  return false if n < 1 || n > 100
   return false unless mochi.all?{|obj| obj.is_a?(Integer)}
   return false if mochi.any?{|num| num < 1 || num > 100}
   true
 end
 
-def kagamimochi(*mochi)
-  return nil unless valid?(*mochi)
+def kagamimochi(n,*mochi)
+  return nil unless valid?(n,*mochi)
   mochi.uniq.length
 end
 
 
-#テスト
+#test
 
-puts kagamimochi(10,8,8,6) == 3
-puts kagamimochi(100,100,100) == 1
-puts kagamimochi(101,100,100) == nil
+puts kagamimochi(4, 10, 8, 8, 6) == 3
+puts kagamimochi(3, 100, 100, 100) == 1
+puts kagamimochi(3, 101, 100, 100) == nil
 puts kagamimochi(0) == nil
 puts kagamimochi("a") == nil
+puts kagamimochi(1, 1) == 1
+puts kagamimochi(1, 0) == nil
+puts kagamimochi(1, "a") == nil
 
-#全てtrue
+#all true
 
 
