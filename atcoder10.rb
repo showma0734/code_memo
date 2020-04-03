bfd9c10c4e01e6606209cbc902a4e8ce11102088
showma0734 <showma0734@gmail.com>
2020-04-03 15:59:34 +0900

    use method

--- atcoder10.rb
+++ atcoder10.rb
@@ -1,26 +1,42 @@
-def valid?(t2,x2,y2)
-  return false unless [t2,x2,y2].all?{|obj| obj.is_a?(Integer)}
+def valid?(n, t2, x2, y2)
+  return false unless [n,t2,x2,y2].all?{|obj| obj.is_a?(Integer)}
+  return false if n < 1 || n > 10**5
+  return false if t2 < 1 || t2 > 10**5
+  return false if x2 < 0 || x2 > 10**5
+  return false if y2 < 0 || y2 > 10**5
   true
 end
 
-def traveling(t2,x2,y2)
-  return nil unless valid?(t2,x2,y2) 
+def traveling(n, t2, x2, y2)
+  return nil unless valid?(n, t2 ,x2 ,y2) 
   t1,x1,y1 = 0,0,0
-  t = t2 - t1
-  d = (x2 - x1).abs + (y2 - y1).abs
-  if d <= t && (t - d).even?
-    result = "YES"
-    t1 = t2
-    x1 = x2
-    y1 = y2
-  else
-    result = "NO" 
-  end
-  p result
+  return "NO" unless calculation(n, t2, x2, y2)
+  "YES"
 end
 
 
+def calculation(n,t2,x2,y2)
+  t1,x1,y1 = 0,0,0
+  n.times do 
+    t = t2 - t1
+    d = (x2 - x1).abs + (y2 - y1).abs
+    if d <= t && (t - d).even?
+      t1,x1,y1 = t2,x2,y2
+    else
+      return 
+    end 
+  end
+  true
+end
 
-puts traveling(2,100,100) == "NO"
-puts traveling(6,1,1) == "YES"
-puts traveling("a","a","a") == nil
+
+#test
+
+puts traveling(1, 2, 100, 100) == "NO"
+puts traveling(2, 6, 1, 1) == "YES"
+puts traveling("a", "a", "a", "a") == nil
+puts traveling(0, 1, 2, 3) == nil
+puts traveling(1, 11**5, 11**5, 11**5) == nil
+puts traveling(1, 10**5, 10**5, 10**5) == "NO"
+
+
