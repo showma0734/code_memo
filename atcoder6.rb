61593884c5b0519d175e9f50cb116756a2e8082b
showma0734 <showma0734@gmail.com>
2020-04-07 18:59:05 +0900

    wrote not until empty

--- atcoder6.rb
+++ atcoder6.rb
@@ -3,6 +3,7 @@ def valid?(n,*cards)
   return false if n < 1 || n > 100
   return false unless cards.all?{|obj| obj.is_a?(Integer)}
   return false if cards.any?{|num| num < 1 || num > 100 }
+  return false if n != cards.length
   true
 end
 
@@ -10,17 +11,24 @@ def cardgame(n,*cards)
   return nil unless valid?(n,*cards)
   alice,bob = 0,0
   cards = cards.sort.reverse
-  until cards.empty? do
-    alice += cards.shift unless cards.size == 0
-    bob += cards.shift unless cards.size == 0
-  end
+  alice = cards.select.with_index{|num, idx| idx % 2 == 0}.sum
+  bob = cards.select.with_index{|num, idx| idx % 2 == 1}.sum
+  
+  #until cards.empty? do
+  #  alice += cards.shift unless cards.size == 0
+  #  bob += cards.shift unless cards.size == 0
+  #end
   p alice - bob
 end
+
+
+
 
 
 # cards.delete(cards.max) if alice += cards.max
 # cards.delete(cards.max) if bob += cards.max
 
+#test
 
 puts cardgame(2,3,1) == 2
 puts cardgame(3,2,7,4) == 5
@@ -31,5 +39,8 @@ puts cardgame(2,101,50) == nil
 puts cardgame(2,"a",50) == nil
 puts cardgame(1,2) == 2
 puts cardgame(0) == nil
-puts cardgame(101) == nil
-puts cardgame(100, 1*100) == 100
+puts cardgame(101, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) == nil
+puts cardgame(100, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1) == 0
+puts cardgame(2 , 3, 4, 5) == nil
+
+#all true
