f8233a3bc4d1ba24412ba8a08efd6f4c112da2fb
showma0734 <showma0734@gmail.com>
2020-04-08 19:03:33 +0900

    wrote simply

--- atcoder9.rb
+++ atcoder9.rb
@@ -6,32 +6,39 @@ end
 
 def dreamerase(s)
   return nil unless valid?(s)
-  return nil unless use_words(s)
-  p use_words(s).length == 0 ? "YES" : "NO"
-end
-
-def order_find(s)
-  s = s.gsub(/eraser/,"").gsub(/erase/,"").gsub(/dreamer/,"").gsub(/dream/,"")
-end
-
-def use_words(s)
   words = ["eraser", "erase", "dreamer", "dream"]
-  return s if words.each {|word| s.gsub!( word, "")}
+  words.each {|word| s.gsub!(word, "")}
+  s.length == 0 ? "YES" : "NO"
 end
+
+#def order_find(s)
+#  s = s.gsub(/eraser/,"").gsub(/erase/,"").gsub(/dreamer/,"").gsub(/dream/,"")
+#end
 
 
 #test
 
 puts dreamerase("dreamerase") == "YES"
+puts dreamerase("erasedream") == "YES"
 puts dreamerase("dreamereraser") == "YES"
+puts dreamerase("eraserdreamer") == "YES"
+puts dreamerase("dreamereraser") == "YES"
+puts dreamerase("erasedreamer") == "YES"
+puts dreamerase("dreameraser") == "YES"
+puts dreamerase("eraserdream") == "YES"
+puts dreamerase("dreamerdream") == "YES"
+puts dreamerase("erasererase") == "YES"
+puts dreamerase("dreamdreamer") == "YES"
+puts dreamerase("eraseeraser") == "YES"
 puts dreamerase("dreamerer") == "NO"
-puts dreamerase(1221) == nil
-puts dreamerase("1221") == "NO"
+puts dreamerase("dreemerase") == "NO"
 puts dreamerase("DREAM") == "NO"
 puts dreamerase("") == nil
 puts dreamerase("a") == "NO"
 puts dreamerase("a"*11**5) == nil
 puts dreamerase("a"*10**5) == "NO"
+puts dreamerase(0123) == nil
+puts dreamerase("1221") == "NO"
 
 #all true
 
