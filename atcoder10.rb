60a6e2b284182b689816a7ae0dfec2f40b715e9e
showma0734 <showma0734@gmail.com>
2020-04-08 18:45:35 +0900

    wrote simply

--- atcoder10.rb
+++ atcoder10.rb
@@ -7,35 +7,37 @@ def valid?(n, t2, x2, y2)
   true
 end
 
+
 def traveling(n, t2, x2, y2)
   return nil unless valid?(n, t2 ,x2 ,y2) 
-  t1,x1,y1 = 0,0,0
-  return "NO" unless calculation(n, t2, x2, y2)
-  "YES"
-end
-
-
-def calculation(n,t2,x2,y2)
-  t1,x1,y1 = 0,0,0
-  n.times do 
-    t = t2 - t1
-    d = (x2 - x1).abs + (y2 - y1).abs
-    if d <= t && (t - d).even?
-      t1,x1,y1 = t2,x2,y2
-    else
-      return 
-    end 
-  end
+  t1,x1,y1 = 0,0,0  
+  t = t2 - t1
+  x = (x2 - x1).abs
+  y = (y2 - y1).abs
+  if t.even? == (x + y).even? && (x + y) <= t
+    t1, x1, y1 = t2, x2, y2
+  else
+    return p "NO"
+  end  
+  p "YES"
 end
 
 
 #test
 
-puts traveling(1, 2, 100, 100) == "NO"
 puts traveling(2, 6, 1, 1) == "YES"
-puts traveling("a", "a", "a", "a") == nil
-puts traveling(0, 1, 2, 3) == nil
-puts traveling(1, 11**5, 11**5, 11**5) == nil
+puts traveling(1, 2, 100, 100) == "NO"
+puts traveling(0, 2, 100, 100) == nil
+puts traveling(10**5, 1,0,1) == "YES"
+puts traveling(11**5, 1,0,1) == nil
 puts traveling(1, 10**5, 10**5, 10**5) == "NO"
+puts traveling(1, 11**5, 11**5, 11**5) == nil
+puts traveling("a", "a", "a", "a") == nil
+puts traveling("a", 1, 1, 1) == nil
+puts traveling(1, "a", 1, 1) == nil
+puts traveling(1, 1, "a", 1) == nil
+puts traveling(1, 1, 1, "a") == nil
+
+#all true
 
 
